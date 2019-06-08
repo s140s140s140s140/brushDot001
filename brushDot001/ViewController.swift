6f0c3900b83da007ff6b0c4638f2371bd601a70e
佐藤一成 <kazunarisato0309@gmail.com>
2019-06-08 10:44:59 +0900

    Initial Commit

---
+++ brushDot001/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  brushDot001
+//
+//  Created by 佐藤一成 on 2019/06/08.
+//  Copyright © 2019 s140. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
